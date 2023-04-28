6c417109f9da7d2261d9f26de1268c1dac54e125
Eunjae TonyLee <lpl2001@naver.com>
2023-04-28 20:13:19 +0900

    several update

---
+++ script.js
@@ -0,0 +1,16 @@
+document.cookie = 'same-site-cookie=foo; SameSite=Lax';
+
+function openClose() {
+    const menu = document.querySelector(".mobile-menu-wrapper");
+    if (menu.style.display == 'flex') {
+        menu.style.display = 'none';
+    }
+    else {
+        menu.style.display = 'flex';
+    }
+
+    const button = document.querySelector(".nav-toggle");
+}
+
+const switcher = document.querySelector('nav-toggle');
+switcher.addEventListener("click", openClose);
